3e93c658bc23322489e60a5448d0e247976f3e9a
AstraLink99 <158212650+AstraLink99@users.noreply.github.com>
2024-12-06 00:54:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def delete_non_matching_csv_files(directory_path, video_id):
 
 st.set_page_config(page_title='Analyzer', page_icon = 'LOGO.png', initial_sidebar_state = 'auto')
 #st.set_page_config(page_title=None, page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
-st.sidebar.title("Sentimental Analsis")
+st.sidebar.title("Sentimental Analysis")
 st.sidebar.header("Enter YouTube Link")
 youtube_link = st.sidebar.text_input("Link")
 directory_path = os.getcwd()
